cfdf619f34093c6535221758dcb460df52a081c5
Jerit Baiju <jeritalumkal@gmail.com>
2023-02-18 14:04:34 +0530

    .

---
+++ main.py
@@ -0,0 +1,11 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop
+
+# load the mnist daataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# reshape and normalize the input data
+x_train = x_train.reshape(60000, 784)
